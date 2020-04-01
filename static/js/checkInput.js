bfba12f2331aac9291a45e2e85dd584acc2089da
inspirit941 <inspirit941@gmail.com>
2020-04-01 15:27:44 +0900

    statis

---
+++ static/js/checkInput.js
@@ -0,0 +1,112 @@
+function checkCountry() {
+  var f = document.getElementById("input-country").value
+  if (f == '') {
+    return false;
+  }
+  return true;
+}
+
+function checkEthn() {
+  var f = document.getElementById("input-ethn").value
+  if (f == '') {
+    return false;
+  }
+  return true;
+}
+
+function checkGender() {
+  var f = document.getElementById("input-gender").value
+  if (f == '') {
+    return false;
+  }
+  return true;
+}
+
+function checkAge() {
+  var f = document.getElementById("input-age").value
+  console.log(Number.isInteger(f))
+  if (f == '' || Number.isInteger(f) || parseInt(f) < 0 || parseInt(f) > 100) {
+    return false;
+  }
+  return true;
+}
+
+function checkHeight() {
+  var f = document.getElementById("input-height").value
+  if (f == ''||Number.isInteger(f) || parseInt(f) < 100 || parseInt(f) > 255){ 
+    if (parseInt(f) == 0) {
+	return true;
+    }
+    return false;
+  }
+  return true;
+}
+
+function checkWeight() {
+  var f = document.getElementById("input-weight").value
+  if (f == ''||Number.isInteger(f) || parseInt(f) < 0 || parseInt(f) > 255) {
+    return false;
+  }
+  return true;
+}
+
+function checkGender() {
+  var f = document.getElementById("input-gender").value
+  if (f == '') {
+    return false;
+  }
+  return true;
+}
+
+
+function checkOccupation() {
+  var f = document.getElementById("input-occupation").value
+  if (f == '') {
+    return false;
+  }
+  return true;
+}
+
+function checkFile() {
+  var f = document.getElementById("inputGroupFile04").value
+  if (f == '') {
+    return false;
+  }
+  return true;
+}
+
+function checkForm() {
+  var warning_color = "#e91e1eb5";
+
+  if (checkCountry() && checkEthn() && checkGender() && checkAge() && checkFile()) return true;
+  else {
+    if(!checkCountry()) document.getElementById("label-country").style.backgroundColor = warning_color;
+    else document.getElementById("label-country").style.backgroundColor = "#e9ecef";
+    
+    if(!checkEthn()) document.getElementById("label-ethn").style.backgroundColor = warning_color;
+    else document.getElementById("label-ethn").style.backgroundColor = "#e9ecef";
+
+    if(!checkGender()) document.getElementById("label-gender").style.backgroundColor = warning_color;
+    else document.getElementById("label-gender").style.backgroundColor = "#e9ecef";
+
+    if(!checkAge()) document.getElementById("label-age").style.backgroundColor = warning_color;
+    else document.getElementById("label-age").style.backgroundColor = "#e9ecef";
+
+    if(!checkHeight()) document.getElementById("label-height").style.backgroundColor = warning_color;
+    else document.getElementById("label-height").style.backgroundColor = "#e9ecef";
+
+    if(!checkWeight()) document.getElementById("label-weight").style.backgroundColor = warning_color;
+    else document.getElementById("label-weight").style.backgroundColor = "#e9ecef";
+
+    if(!checkFile()) document.getElementById("label-file").style.backgroundColor = warning_color;
+    else document.getElementById("label-file").style.backgroundColor = "#e9ecef";
+
+    
+    if(!checkOccupation()) document.getElementById("label-occupation").style.backgroundColor = warning_color;
+    else document.getElementById("label-occupation").style.backgroundColor = "#e9ecef";
+
+
+    alert("Some values are invalid.");
+    return false;
+  }
+}
